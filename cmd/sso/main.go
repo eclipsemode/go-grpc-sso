ed45ee99e7d9e983213d97810562de62b18e8bd1
eclipsemode <eclipsemod-post@yandex.ru>
2024-07-16 23:21:12 +0300

    initial/config/logger

---
+++ cmd/sso/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"github.com/eclipsemode/go-grpc-sso/internal/config"
+	"github.com/eclipsemode/go-grpc-sso/internal/lib/logger/handlers/slogpretty"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	//TODO: инициализировать обьект кофига
+	cfg := config.MustLoad()
+
+	//TODO: инициализировать логгер
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting server", slog.Any("cfg", cfg))
+	//TODO: инициализировать приложение (app)
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = setupPrettySlog()
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
+}
