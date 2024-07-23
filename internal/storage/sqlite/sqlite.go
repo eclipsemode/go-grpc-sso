0eb78d1d758cb642e2b800f722b886ab6c1ce9dd
eclipsemode <eclipsemod555@gmail.com>
2024-07-24 00:33:16 +0300

    register-login tests

--- internal/storage/sqlite/sqlite.go
+++ internal/storage/sqlite/sqlite.go
@@ -64,7 +64,7 @@ func (s *Storage) SaveUser(ctx context.Context, email string, passHash []byte) (
 func (s *Storage) User(ctx context.Context, email string) (models.User, error) {
 	const op = "storage.sqlite.User"
 
-	stmt, err := s.db.Prepare("SELECT * FROM users WHERE email = ?")
+	stmt, err := s.db.Prepare("SELECT id, email, pass_hash FROM users WHERE email = ?")
 	if err != nil {
 		return models.User{}, fmt.Errorf("%s: %w", op, storage.ErrUserNotFound)
 	}
@@ -77,7 +77,7 @@ func (s *Storage) User(ctx context.Context, email string) (models.User, error) {
 
 	var user models.User
 
-	err = stmt.QueryRowContext(ctx, email).Scan(&user.ID, &user.Email)
+	err = stmt.QueryRowContext(ctx, email).Scan(&user.ID, &user.Email, &user.PassHash)
 	if errors.Is(err, sql.ErrNoRows) {
 		return models.User{}, fmt.Errorf("%s: %w", op, storage.ErrAppNotFound)
 	}
--- tests/auth_register_login_test.go
+++ tests/auth_register_login_test.go
@@ -1,8 +1,14 @@
 package tests
 
 import (
+	"github.com/brianvoe/gofakeit/v6"
+	ssov1 "github.com/eclipsemode/go-grpc-sso-protobuf/gen/go/sso"
 	"github.com/eclipsemode/go-grpc-sso/tests/suite"
+	"github.com/golang-jwt/jwt"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"testing"
+	"time"
 )
 
 const (
@@ -10,9 +16,53 @@ const (
 	appID      = 1
 	appSecret  = "test-secret"
 
-	passDefaultLet = 10
+	passDefaultLength = 10
 )
 
 func TestRegisterLogin_Login_HappyPath(t *testing.T) {
 	ctx, st := suite.New(t)
+
+	email := gofakeit.Email()
+	password := randomFakePassword()
+
+	respReg, err := st.AuthClient.Register(ctx, &ssov1.RegisterRequest{
+		Email:    email,
+		Password: password,
+	})
+
+	require.NoError(t, err)
+	assert.NotEmpty(t, respReg.GetUserId())
+
+	respLogin, err := st.AuthClient.Login(ctx, &ssov1.LoginRequest{
+		Email:    email,
+		Password: password,
+		AppId:    appID,
+	})
+
+	require.NoError(t, err)
+
+	loginTime := time.Now()
+
+	token := respLogin.GetToken()
+	require.NotEmpty(t, token)
+
+	tokenParsed, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		return []byte(appSecret), nil
+	})
+	require.NoError(t, err)
+
+	claims, ok := tokenParsed.Claims.(jwt.MapClaims)
+	require.True(t, ok)
+
+	assert.Equal(t, respReg.GetUserId(), int64(claims["uid"].(float64)))
+	assert.Equal(t, email, claims["email"].(string))
+	assert.Equal(t, appID, int(claims["app_id"].(float64)))
+
+	const deltaSeconds = 1
+
+	assert.InDelta(t, loginTime.Add(st.Cfg.TokenTTL).Unix(), claims["ext"].(float64), deltaSeconds)
+}
+
+func randomFakePassword() string {
+	return gofakeit.Password(true, true, true, true, false, passDefaultLength)
 }
